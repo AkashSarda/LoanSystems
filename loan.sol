b87e2dbc0f1639a16a7da0f726a7c05e7e4d3b3b
Akash Sarda <akashsarda3@gmail.com>
2017-06-08 00:11:32 +0530

    added structs

--- loan.sol
+++ loan.sol
@@ -1,10 +1,38 @@
 pragma solidity ^0.4.0;
 
 contract System{
+    uint256 loanid;
+
+    struct Loan{
+      uint timestamp;
+      uint amount;
+      bool active;
+    }
+
+    struct AccountEntry{
+      uint timestamp;
+      uint amount;
+    }
+
+
+    struct Lenders{
+      uint balance;
+      uint timestamp_of_creation;
+      AccountEntry[] deposits;
+      AccountEntry[] withdrawals;
+    }
+
+    struct Borrowers{
+      uint amount;
+      uint timestamp;
+      bool borrowed;
+      mapping(uint256 => Loan) loans;
+      AccountsEntry[] moneygivenback;
+    }
 
     // mapping stores
-    mapping(address => uint) lenders;
-    mapping(address => uint) borrowers;
+    mapping(address => Lenders) lenders;
+    mapping(address => Borrowers) borrowers;
 
     event Deposit(address lender, uint amount);
     event DebtPaid(address lender_addrs, uint lender_bal,address sender_addrs,uint sender_bal);
@@ -15,27 +43,35 @@ contract System{
 
     function System(){
       owner = msg.sender;
+      loanid = 0;
     }
 
     function registerLender(){
-      lenders[msg.sender] = 0;
+      lenders[msg.sender].balance = 0;
+      lenders[msg.sender].timestamp_of_creation = now;
     }
 
     function registerBorrower(){
-      borrowers[msg.sender] = 0;
+      borrowers[msg.sender].amount = 0
+      borrowers[msg.sender].timestamp_of_creation = now;
+      borrowers[msg.sender].borrowed = false;
     }
 
     function lend(uint money) payable{
-      lenders[msg.sender] += money;
+      lenders[msg.sender].balance += money;
+      lenders[msg.sender].deposits.push(AccountEntry(now, money));
       Desposit(msg.sender, money);
     }
 
     function borrow_money(uint money, address lender_address) returns (bool) {
-      if(lenders[lender_address] > money) {
+      if(lenders[lender_address] > money && borrowers[msg.sender].borrowed == false) {
         if(msg.sender.send(money)){
           valid = true;
 //          lenders[lender_address] -= money;
-          borrowers[msg.sender] += money;
+          borrowers[msg.sender].borrowed = true;
+          borrowers[msg.sender].amount += money;
+          borrowers[msg.sender].loans[loanid].amount = money;
+          borrowers[msg.sender].timestamp = now;
           Money_Borrowed(lender_address,lender_address.balance,msg.sender,msg.sender.balance);
         }else{
           valid = false;
@@ -52,14 +88,20 @@ contract System{
         return addrs.balance;
     }
 
-    function pay_debts(address lender_address, uint money) returns (bool){
-      if (money >= 0) {
+    function pay_debts(address lender_address, uint money) payable returns (bool){
+      if (money >= 0 && borrowers[msg.sender].borrowed == true) {
 
         if(lender_address.send(money)){
 
-          DebtPaid(lender_address,lender_address.balance,msg.sender,msg.sender.balance);
-          borrowers[msg.sender] -= money;
+          borrowers[msg.sender].amount -= money;
+
+          if (borrowers[msg.sender].amount == 0){
+            borrowers[msg.sender].borrowed = false;
+          }
+
           valid = true;
+          DebtPaid(lender_address,lender_address.balance,msg.sender,msg.sender.balance);
+
         }else{
           valid = false;
         }
