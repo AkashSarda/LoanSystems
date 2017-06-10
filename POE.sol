6e31ad3448cf22bc1f6a8b5e6a94f066cfac961b
Akash Sarda <akashsarda3@gmail.com>
2017-06-10 18:18:25 +0530

    added leasing function in poe.sol

--- POE.sol
+++ POE.sol
@@ -2,11 +2,13 @@ pragma solidity ^0.4.4;
 
 // This is contract is assumed to be maintained globally for verification purposes.
 
-
 contract POE {
   address public owner;
-
-  mapping (bytes32 => address) asset_docs;
+  struct Asset{
+    address asset_owner;
+    bool leased = false;
+  }
+  mapping (bytes32 => Asset) asset_docs;
 
   function POE() {
     // constructor
@@ -15,12 +17,12 @@ contract POE {
 
 // to add someone's asset.
   function storeProof(address asset_owner, bytes32 dochash){
-    asset_docs[dochash] = asset_owner;
+    asset_docs[dochash].asset_owner = asset_owner;
   }
 
 // the bank will verify the ownership and then proceed with the loan.
   function verify(address asset_owner, bytes32 dochash) constant returns (bool){
-    if (asset_docs[dochash] == asset_owner) {
+    if (asset_docs[dochash].asset_owner == asset_owner) {
       return true;
     }else{
       return false;
@@ -36,4 +38,15 @@ contract POE {
       return false;
     }
   }
+
+// give on leased
+  function giveOnLease(bytes32 dochash) returns (bool){
+    if (msg.sender == asset_docs[dochash].asset_owner) {
+      asset_docs[dochash].leased = true;
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
 }
