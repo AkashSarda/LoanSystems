92e172b68bc952e03559b295cf413f733776d282
Vijesh Ghandare <ghandarevb14.comp@coep.ac.in>
2017-06-12 14:12:36 +0530

    added borrower table & deadline entry

--- NodeJS/initdb.js
+++ NodeJS/initdb.js
@@ -18,11 +18,15 @@ connection.connect(function(err) {
    if (err) throw err;
    console.log("Database created");
  });*/
+
   //var sel_db = "USE DATABASE Loansystem";
   var tbl1 = "CREATE TABLE lenders (address VARCHAR(255) primary key,time_stamp DATE, balance INTEGER)";
   var tbl2 = "CREATE TABLE deposits (address VARCHAR(255) primary key, time_stamp DATE, amount INTEGER)";
   var tbl3 = "CREATE TABLE withdraws (address VARCHAR(255) primary key, time_stamp DATE, amount INTEGER)";
   var tbl4 = "CREATE TABLE mapping (name VARCHAR(255),address VARCHAR(255))";
+  var tbl5 = "CREATE TABLE Borrower(address VARCHAR(255) primary key, time_stamp DATE, Deadline DATE,balance INTERGER)";
+  var tbl6 = "CREATE TABLE Loan(loan_id INTEGER,address VARCHAR(255),amount INTEGER,loan_cash INTEGER, time_stamp DATE");
+  var tbl7 = "CREATE TABLE Payback(address VARCHAR(255),amount INTEGER,loan_id INTEGER,loan_cash INTEGER, time_stamp DATE");
 
 /*connection.query(sel_db, function (err, result) {
      if (err) throw err;
@@ -50,3 +54,15 @@ connection.query(tbl4, function (err, result) {
     });
 connection.end();
 });
+connection.query(tbl5, function (err, result) {
+   if (err) throw err;
+   console.log("Borrower Table created");
+ });
+connection.query(tbl6, function (err, result) {``
+   if (err) throw err;
+   console.log("Loan Table created");
+ });
+connection.query(tbl7, function (err, result) {
+   if (err) throw err;
+   console.log("Payback Table created");
+ });
--- NodeJS/maindb.js
+++ NodeJS/maindb.js
@@ -14,33 +14,64 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 
   function addLender(address,time_stamp,balance){
-    var query = "INSERT INTO lenders (address,time_stamp,balance) VALUES (address,time_stamp,balance)";
+    var query = "INSERT INTO lenders (address,time_stamp,balance) VALUES ('"+address+"','"+time_stamp+"','"+balance+"')";
     connection.query(query,function (err, result) {
     if (err) throw err;
     console.log("Number of records inserted: " + result.affectedRows);
     });
   }
   function addDeposit(address,time_stamp,amount){
-    var query = "INSERT INTO deposits (address,time_stamp,amount) VALUES (address,time_stamp,amount)";
+    var query = "INSERT INTO deposits (address,time_stamp,amount) VALUES  ( '"+ address +"', '" + time_stamp + "', '" + amount + "')";
     connection.query(query,function (err, result) {
     if (err) throw err;
     console.log("Number of records inserted: " + result.affectedRows);
     });
   }
   function addWithdraw(address,time_stamp,amount){
-    var query = "INSERT INTO withdraws (address,time_stamp,amount) VALUES (address,time_stamp,amount)";
+    var query = "INSERT INTO withdraws (address,time_stamp,amount) VALUES ('"+address+"','"+time_stamp+"','"+amount+"')";
     connection.query(query,function (err, result) {
     if (err) throw err;
     console.log("Number of records inserted: " + result.affectedRows);
     });
   }
   function addMapping(name,address){
-    var query = "INSERT INTO mapping (name,address) VALUES (name,address)";
+    var query = "INSERT INTO mapping (name,address) VALUES ('"+ name + "','"+ address+"')";
+    console.log(query);
+    connection.query(query,function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    });
+  }
+  function addBorrower(address,time_stamp,deadline,balance){
+    var query = "INSERT INTO Borrower (address,time_stamp,Deadline,balance) VALUES ('"+address+"','"+time_stamp+"','"+deadline+"','"+balance+"')";
+    console.log(query);
+    connection.query(query,function(err,result){
+      if(err) throw err;
+      console.log("Number of records inserted:" + result.affectedRows);
+    });
+  }
+
+  function addLoan(loan_id,address,amount,loan_cash,time_stamp){
+    var query = "INSERT INTO Loan (loan_id,address,amount,loan_cash,time_stamp) VALUES ('"+loan_id+"','"+address+"','"+amount+"','"+loan_cash+"','"+time_stamp+"')";
+    console.log(query);
+    connection.query(query,function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    });
+  }
+  function addPayback(address,amount,loan_id,loan_cash,time_stamp){
+    var query = "INSERT INTO Loan (address,amount,loan_id,loan_cash,time_stamp) VALUES ('"+address+"','"+amount+"','"+loan_id+"','"+loan_cash+"','"+time_stamp+"')";
+    console.log(query);
     connection.query(query,function (err, result) {
     if (err) throw err;
     console.log("Number of records inserted: " + result.affectedRows);
     });
   }
 
+  console.log(addMapping("Alice","9850056502"));
+  connection.query("SELECT * FROM mapping", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
   connection.end();
 });
