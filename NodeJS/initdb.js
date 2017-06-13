8b69ff87f2dc1d24165d06e4a0b603c52cdbfa1e
Vijesh <ghandarevb14.comp@coep.ac.in>
2017-06-14 00:42:02 +0530

    "changed password"

--- NodeJS/initdb.js
+++ NodeJS/initdb.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '12422',
+  password : '12345',
   database : 'Loansystem'
 });
 
