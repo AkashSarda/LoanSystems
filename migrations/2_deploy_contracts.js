22e531e148b4f8b6e97cdd478b58fdd45f1fd821
Vijesh Ghandare <ghandarevb14.comp@coep.ac.in>
2017-07-23 13:06:33 +0530

    updated code with asset

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,6 @@
 var System = artifacts.require("./System.sol");
+var Asset = artifacts.require("./Asset.sol");
 module.exports = function(deployer) {
   deployer.deploy(System);
+  deployer.deploy(Asset);
 };
--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 var express = require('express');
+var Web3 = require('web3');
 var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs'); //extension of views
 app.use(express.static(__dirname + '/public'));
 app.set('port', process.env.PORT || 8000);
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
 app.get('/lender', function(req,res){
     res.render('lender');
 });
 app.get('/borrower',function(req,res){
     res.render('borrower');
+});
+app.get('/asset',function(req,res){
+      res.render('asset');
+  });
+  app.post('/asset',urlencodedParser,function(req,res){
+      console.log(req.body);
+      var hash = web3.sha3(req.body.firstName + req.body.lastName + req.body.hashValue + req.body.date + req.body.secId);
+      console.log(hash);
+      res.render('asset');
+  });
+app.get('/verify',function(req,res){
+  res.render('verify');
 });
 // custom 404 page
 app.use(function(req, res){
